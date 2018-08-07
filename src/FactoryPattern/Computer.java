07c21fb107e3bf9bedbd29acfc43c58a7d19d128
brucewilliamwaynes <mukherjee.anirbanmukherjee@gmail..com>
2018-08-07 11:33:34 +0000

    Factory Pattern Sample

---
+++ src/FactoryPattern/Computer.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package FactoryPattern;
+
+/**
+ * @author bridgelabz
+ *
+ */
+public abstract class Computer {
+
+	public abstract String getRAM();
+	public abstract String getHDD();
+	public abstract String getCPU();
+	
+	@Override
+	public String toString(){
+		return "RAM= "+this.getRAM()+", HDD="+this.getHDD()+", CPU="+this.getCPU();
+	}
+	
+}
---
+++ src/FactoryPattern/ComputerFactory.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package FactoryPattern;
+
+/**
+ * @author bridgelabz
+ *
+ */
+public class ComputerFactory {
+
+	public static Computer getComputer(String type, String ram, String hdd, String cpu){
+		if("PC".equalsIgnoreCase(type)) return new PC(ram, hdd, cpu);
+		else if("Server".equalsIgnoreCase(type)) return new Server(ram, hdd, cpu);
+		
+		return null;
+	}
+	
+	
+}
